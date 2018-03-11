a35e8cee51eebfd7733a0ef85d75d4ee7ef4493f
Mats Törnberg <mats.tornberg@gmail.com>
2018-03-11 18:05:05 +0100

    parser via refelction

--- telldus.parser/Models/ConfigFile.cs
+++ telldus.parser/Models/ConfigFile.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace telldusconf.Models
 {
     public class ConfigFile
     {
         [Key("user")]
         public string User { get; set; }
+
+        [ListKey("device")]
+        public List<Device> Devices { get; set; }
 
         public ConfigFile()
         {
--- telldus.parser/Parsing/Parser.cs
+++ telldus.parser/Parsing/Parser.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using telldusconf.Models;
+
 namespace telldusconf.Parsing
 {
     public class Parser
@@ -8,12 +14,35 @@ namespace telldusconf.Parsing
         public Parser(string filename)
         {
             _filePath = filename;
-
+            var fileStream = new FileStream(_filePath, FileMode.Open);
+            Parse(fileStream);
         }
 
-        public ConfigFile Parse(StreamReader reader)
+        public ConfigFile Parse(FileStream fileStream)
         {
+            var ret = new ConfigFile();
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+                ret = ParseObject<ConfigFile>(reader);
+            }
+            return ret;
+        }
 
+        private T ParseObject<T>(StreamReader reader)
+        {
+            var ret = Activator.CreateInstance<T>();
+            var type = typeof(T);
+            var prps = type.GetProperties();
+            var keyDict = new Dictionary<string, PropertyInfo>();
+            foreach (var prp in prps)
+            {
+                var key = prp.GetCustomAttributes(true).OfType<KeyAttribute>().FirstOrDefault();
+                if (key != null)
+                {
+                    keyDict.Add(key.Key, prp);
+                }
+            }
+            return ret;
         }
     }
 }
