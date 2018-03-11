bd3bb164b5f67759f68865b21ecaa5fd96c45bbb
Mats Törnberg <mats.tornberg@gmail.com>
2018-03-11 17:51:50 +0100

    parser base

---
+++ telldus.parser/Models/ConfigFile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace telldusconf.Models
+{
+    public class ConfigFile
+    {
+        [Key("user")]
+        public string User { get; set; }
+
+        public ConfigFile()
+        {
+        }
+    }
+}
---
+++ telldus.parser/Models/KeyAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace telldusconf.Models
+{
+    public class KeyAttribute : Attribute
+    {
+        private string _key;
+
+        public string Key { get { return _key; } }
+
+        public KeyAttribute(string key)
+        {
+            _key = key;
+        }
+    }
+
+    public class ObjectKeyAttribute : KeyAttribute
+    {
+        public ObjectKeyAttribute(string key) : base(key)
+        {
+        }
+    }
+}
